89cfd15b31efb3b26fe6f01f693347e208ac3496
root <root@DESKTOP-1PF4AT3.localdomain>
2022-11-02 22:17:40 +0900

    signup

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.frontpage, name='frontpage'),
+    path('signup/', views.signup, name='signup'),
 ]
