6324840ba673b67f6cc88192fa48aef938ad492e
root <root@DESKTOP-1PF4AT3.localdomain>
2022-11-02 22:39:28 +0900

    login

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.frontpage, name='frontpage'),
     path('signup/', views.signup, name='signup'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
 ]
