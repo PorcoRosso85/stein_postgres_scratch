e6fe1e696cd50c48e722b17f7515532a1fc10a56
root <root@DESKTOP-1PF4AT3.localdomain>
2022-11-03 16:49:59 +0900

    logout, rooms, superuser

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.frontpage, name='frontpage'),
     path('signup/', views.signup, name='signup'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    # //TODO loginbutton should take us to rooms, but now it takes us to 'account...' page
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
