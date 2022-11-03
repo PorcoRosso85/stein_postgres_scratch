29a5262bc22878412c26f1401f958ec498374e34
root <root@DESKTOP-1PF4AT3.localdomain>
2022-11-03 16:47:08 +0900

    logout, rooms, superuser

---
+++ room/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Room
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def rooms(request):
+    rooms = Room.objects.all()
+    return render(request, 'rooms.html', {'rooms': rooms})
