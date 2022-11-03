7cf33c98daa1e5ddac79e81ad0c2fc68216bb7fb
root <root@DESKTOP-1PF4AT3.localdomain>
2022-11-03 17:13:43 +0900

    sampled room, before js

--- room/views.py
+++ room/views.py
@@ -6,3 +6,8 @@ from django.contrib.auth.decorators import login_required
 def rooms(request):
     rooms = Room.objects.all()
     return render(request, 'rooms.html', {'rooms': rooms})
+
+@login_required
+def room(request, slug):
+    room = Room.objects.get(slug=slug)
+    return render(request, 'room.html', {'room': room})
